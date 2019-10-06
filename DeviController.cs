b86bab0ffce548c215c60f8e2385bac469a3d594
TennysonBranch <54789228+TennysonBranch@users.noreply.github.com>
2019-10-05 22:26:29 -0400

    Add files via upload

---
+++ DeviController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeviController : MonoBehaviour
+{
+    public AudioSource musicSource;
+
+
+    public AudioClip musicClipOne;
+
+
+    public AudioClip musicClipTwo;
+
+
+    Animator anime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anime = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            musicSource.clip = musicClipOne;
+            musicSource.Play();
+            anime.SetInteger("State", 1);
+        }
+        if(Input.GetKeyUp(KeyCode.W))
+        {
+            musicSource.Stop();
+            anime.SetInteger("State", 0);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            musicSource.clip = musicClipTwo;
+            musicSource.Play();
+            anime.SetInteger("State", 2);
+        }
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            musicSource.Stop();
+            anime.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            musicSource.loop = true;
+        }
+        if (Input.GetKeyUp(KeyCode.L))
+        {
+            musicSource.loop = false;
+        }
+    }
+}
